d4cc5fad67f19968d17c2d5d1f8d30bf800fcf4c
rprendergast180 <rprendergast1121@gmail.com>
2020-09-19 15:19:29 -0700

    Initialized simple flask backend

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def asset_default():
+    return "Default asset"
+
+@app.route('/<id>')
+def asset_id(id):
+    return id
+
+if __name__ == '__main__':
+    app.run()
+
