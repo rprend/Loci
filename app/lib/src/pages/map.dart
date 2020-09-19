bbd75bd99f34691eda517cbad47d988ef137be79
Theodore Li <theodoreqili@gmail.com>
2020-09-18 23:08:59 -0500

    Add basic sprite

--- app/lib/src/pages/map.dart
+++ app/lib/src/pages/map.dart
@@ -1,8 +1,48 @@
+import 'package:flame/components/component.dart';
+import 'package:flame/components/mixins/has_game_ref.dart';
+import 'package:flame/game/base_game.dart';
+import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
+import 'package:flame/flame.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  MyGame game;
+
+  @override
+  void initState() {
+    super.initState();
+    game = MyGame();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('hello'),
+        ),
+        body: game.widget);
+  }
+}
+
+class MyCrate extends SpriteComponent {
+  // creates a component that renders the crate.png sprite, with size 16 x 16
+  MyCrate() : super.fromSprite(16.0, 16.0, new Sprite('building.png'));
+
+  @override
+  void resize(Size size) {
+    // we don't need to set the x and y in the constructor, we can set then here
+    this.x = (size.width - this.width) / 2;
+    this.y = (size.height - this.height) / 2;
+  }
+}
+
+class MyGame extends BaseGame {
+  MyGame() {
+    add(MyCrate());
   }
 }
