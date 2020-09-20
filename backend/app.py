d3fdbf378fa1f2c20c3072a476331878678e95d7
rprendergast180 <rprendergast1121@gmail.com>
2020-09-19 22:18:08 -0700

    Upload file to server by POSTing at /scan

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import flask
 from flask import request
+from werkzeug.utils import secure_filename
 import os
 import tmxlib
 
@@ -8,13 +9,18 @@ import asyncio
 loop = asyncio.get_event_loop()
 app = flask.Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = './img/'
+
 async def construct_from_image(image):
     await asyncio.sleep(2)
-    return "hell yeah bitch we goooood!"
+    return "Async return"
 
 @app.route('/scan', methods=['POST'])
 def scan_image():
-    image = request.json['img']
+    image = request.files['img']
+    filename = secure_filename(image.filename)
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+
     print(image)
     result = loop.run_until_complete(construct_from_image(image))
 
